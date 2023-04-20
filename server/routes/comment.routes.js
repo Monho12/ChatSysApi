1b8ee245efd5f5f9e57e78059cf741935180c17a
Monho12 <oorgilmojo@gmail.com>
2023-04-20 22:51:45 +0800

    comment crud added [bug fixed]

--- server/routes/comment.routes.js
+++ server/routes/comment.routes.js
@@ -1,11 +1,10 @@
 const express = require("express");
 const {
-  createComment,
-  deleteComment,
   getComments,
+  createComment,
   addCommentsToPost,
-} = require("../controller/comment.controller");
-const { roleMiddleware } = require("../middlewares/roleMiddleware");
+  deleteComment,
+} = require("../controllers/comment.controller");
 
 const router = express.Router();
 
@@ -13,6 +12,6 @@ router
   .get("/comments", getComments)
   .post("/create", createComment)
   .put("/add", addCommentsToPost)
-  .delete("/comment/:id", roleMiddleware, deleteComment);
+  .delete("/comment/:id", deleteComment);
 
 module.exports.commentRoutes = router;
